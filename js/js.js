8886c87b10610023875f4200bf10b6f1c138163e
moniquetrotta <monique.trotta@gmail.com>
2022-01-08 20:10:28 -0300

    adiciona disparo

--- js/js.js
+++ js/js.js
@@ -9,6 +9,7 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
   //Principais variáveis do jogo
+	var podeAtirar=true;
 	var jogo = {}	
 	var velocidade=5;
 var posicaoY = parseInt(Math.random() * 334);
@@ -36,7 +37,10 @@ var posicaoY = parseInt(Math.random() * 334);
 	function loop() {	
 	movefundo();	
 	movejogador();
-	moveinimigo1();
+	moveinimigo1();	
+	moveinimigo2();
+	moveamigo();
+
 	} // Fim da função loop()
 
   //Função que movimenta o fundo do jogo	
@@ -69,8 +73,8 @@ var posicaoY = parseInt(Math.random() * 334);
 			}
 			
 			if (jogo.pressionou[TECLA.D]) {
-				
-				//Chama função Disparo	
+				disparo(); 	//Chama função Disparo	
+			
 			}
 		
 			} // fim da função movejogador()
@@ -87,5 +91,59 @@ var posicaoY = parseInt(Math.random() * 334);
 						
 					}
 			} //Fim da função moveinimigo1()
+			function moveinimigo2() {
+        posicaoX = parseInt($("#inimigo2").css("left"));
+	$("#inimigo2").css("left",posicaoX-3);
+				
+		if (posicaoX<=0) {
 			
+		$("#inimigo2").css("left",775);
+					
+		}
+} // Fim da função moveinimigo2()
+function moveamigo() {
+	
+	posicaoX = parseInt($("#amigo").css("left"));
+	$("#amigo").css("left",posicaoX+1);
+				
+		if (posicaoX>906) {
+			
+		$("#amigo").css("left",0);
+					
+		}
+
+} // fim da função moveamigo()
+function disparo() {
+	
+	if (podeAtirar==true) {
+	
+		podeAtirar=false;
+
+	topo = parseInt($("#jogador").css("top"))
+	posicaoX= parseInt($("#jogador").css("left"))
+	tiroX = posicaoX + 190;
+	topoTiro=topo+37;
+	$("#fundoGame").append("<div id='disparo'></div");
+	$("#disparo").css("top",topoTiro);
+	$("#disparo").css("left",tiroX);
+	
+	var tempoDisparo=window.setInterval(executaDisparo, 30);
+	
+	} //Fecha podeAtirar
+ 
+   	    function executaDisparo() {
+	    posicaoX = parseInt($("#disparo").css("left"));
+	    $("#disparo").css("left",posicaoX+15); 
+
+        		if (posicaoX>900) {
+						
+			window.clearInterval(tempoDisparo);
+			tempoDisparo=null;
+			$("#disparo").remove();
+			podeAtirar=true;
+					
+                   }
+	} // Fecha executaDisparo()
+} // Fecha disparo()
+
 } // Fim da função start
