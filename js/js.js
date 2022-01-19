ba7518efe90f1d3a87f32fffaeb1b1a5b662e890
moniquetrotta <monique.trotta@gmail.com>
2022-01-19 09:39:51 -0300

    Velocidade/dificuldade inimigo

--- js/js.js
+++ js/js.js
@@ -186,6 +186,7 @@ function start() { // Inicio da função start()
 		// Disparo com o inimigo1
 
 		if (colisao3.length > 0) {
+			velocidade=velocidade+0.5;
 			pontos=pontos+100;
 			inimigo1X = parseInt($("#inimigo1").css("left"));
 			inimigo1Y = parseInt($("#inimigo1").css("top"));
